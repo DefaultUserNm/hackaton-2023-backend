20814e8cf683481203ccf4fe4c655c91ffda216e
Alena Nepryakhina <AANepryakhina@sberbank.ru>
2023-06-15 22:06:01 +0300

    Add test and missing components

--- hackaton-app/src/test/groovy/it/DocumentRepositoryIT.groovy
+++ hackaton-app/src/test/groovy/it/DocumentRepositoryIT.groovy
@@ -6,11 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import ru.sbrf.hackaton.app.Application
+import ru.sbrf.hackaton.app.mapper.ComponentMapper
+import ru.sbrf.hackaton.app.mapper.ProductMapper
 import ru.sbrf.hackaton.app.model.domain.entity.DocumentEntity
+import ru.sbrf.hackaton.app.model.domain.entity.ProductEntity
+import ru.sbrf.hackaton.app.model.dto.ComponentDTO
+import ru.sbrf.hackaton.app.model.dto.ProductDTO
+import ru.sbrf.hackaton.app.repository.ComponentRepository
 import ru.sbrf.hackaton.app.repository.DocumentRepository
+import ru.sbrf.hackaton.app.repository.ProductRepository
+import ru.sbrf.hackaton.app.service.ProductService
+import ru.sbrf.hackaton.app.service.impl.ProductServiceImpl
 import spock.lang.Specification
 
+import static common.testDataBuilder.DocumentEntityDataBuilder.buildComponentDTO
 import static common.testDataBuilder.DocumentEntityDataBuilder.buildDocumentEntity
+import static common.testDataBuilder.DocumentEntityDataBuilder.buildProductDTO
+import static common.testDataBuilder.DocumentEntityDataBuilder.buildProductEntity
 
 @ActiveProfiles("TEST")
 @SpringBootTest(
@@ -24,6 +36,16 @@ class DocumentRepositoryIT extends Specification {
 
     @Autowired
     DocumentRepository documentRepository
+    @Autowired
+    ComponentRepository componentRepository
+    @Autowired
+    ProductRepository productRepository
+    @Autowired
+    ProductServiceImpl productServiceImpl
+    @Autowired
+    ProductMapper productMapper
+    @Autowired
+    ComponentMapper componentMapper
     @Autowired
     DbCleaner dbCleaner
 
@@ -51,5 +73,18 @@ class DocumentRepositoryIT extends Specification {
 
         then:
         actual == expected
+    }
+
+    def "Test getProduct (1) "() {
+        given:
+        ProductEntity productEntity = buildProductEntity()
+        componentRepository.saveAll(productEntity.componentEntities)
+        productRepository.save(productEntity)
+
+        when:
+        ProductDTO actual = productServiceImpl.getProduct(productEntity.id)
+
+        then:
+        actual == productMapper.toProductDTO(productEntity)
     }
 }
